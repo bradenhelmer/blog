e1734875214ef8d0648a77510c44c9bf52cbef2a
Braden Helmer <bradenhelmeraus@gmail.com>
2024-12-22 11:13:42 -0500

    Layout and routes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,51 @@
+<script lang="ts" charset="utf-8">
+	let { children } = $props();	
+</script>
+<style type="text/css" media="screen">
+	.nav_bar {
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		grid-template-rows: repeat(2, 1fr);
+		padding: 1rem;
+	}
+	.nav_bar div {
+		text-align: center;
+		padding: 0.5rem 1rem;
+	}
+	.nav_bar a {
+		text-align: center;
+		padding: 0.5rem 1rem;
+	}
+	.blog_title {
+		grid-column-start: 1;
+		grid-column-end: 4;
+	}
+
+</style>
+
+<div class="page">
+	<div class="nav_bar">
+		<div class="blog_title">
+		<a href="/">
+			BH Blog
+		</a>
+		</div>
+		<div>
+			Socials	
+		</div>
+		<a href="/about">
+			About	
+		</a>
+		<a href="/posts">
+			Posts
+		</a>
+		<a href="/projects">
+			Projects
+		</a>
+		<a href="/resume">
+			Resume
+		</a>
+	</div>
+</div>
+
+{@render children()}
